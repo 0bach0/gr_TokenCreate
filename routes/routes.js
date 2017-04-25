aa58c6cc4eef26b81c3a8a16582d6decbd202542
bachnx <bachnx.hedspi@gmail.com>
2017-04-25 11:49:09 +0000

    first semester

--- routes/routes.js
+++ routes/routes.js
@@ -11,18 +11,20 @@ var appRouter = function(app) {
         var options = {
             host: 'graph.facebook.com',
             path: "oauth/access_token?client_id=" + appInfo.appID + "&client_secret="+appInfo.appSecret +
-            "&grant_type=fb_exchange_token&fb_exchange_token=" + req.query.accessToken
+            "&grant_type=fb_exchange_token&fb_exchange_token=" + req.query.access_token
         };
         
-        
+        var userID = req.query.id;
         var request = require('request');
         var url = 'https://graph.facebook.com/' + options.path;
+        
         console.log(url);
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log("Receive from FB",body); 
-                
-                var url2="http://tokenmanager:3000/newtoken?loginname=" + req.query.email + '&' + body;
+                body = JSON.parse(body);
+                var url2="http://tokenmanager:3000/newtoken?id="+ userID +'&access_token=' + body.access_token +'&expires=' + body.expires_in;
+                console.log(url2);
                 request(url2, function (error, response, body) {
                     if (!error && response.statusCode == 200) {
                         console.log("Receive from Token Manager",body); 
